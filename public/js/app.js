2af4d42a16c22ba696b8c8225a7c5ad943e11967
Jin Woo Kim <jinwoov@gmail.com>
2020-02-07 01:16:32 -0800

    fixed js file and css

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,9 @@
 
 $('#profile-photo').mouseover(() => {
   $('#name').css("font-weight", "bold");
+  $('#name').css("font-size", "1.6em");
 });
 $('#profile-photo').mouseleave(() => {
   $('#name').css("font-weight", "normal");
+  $('#name').css("font-size", "1.5em");
 });
