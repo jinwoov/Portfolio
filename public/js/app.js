5951217fa2866e64b72babae2376077fa5667a5a
Jin Woo Kim <jinwoov@gmail.com>
2020-02-07 09:42:30 -0800

    added functionality to the aside bar

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,11 @@ $('#profile-photo').mouseleave(() => {
   $('#name').css("font-weight", "normal");
   $('#name').css("font-size", "1.5em");
 });
+
+$(document).scroll(function() {
+  if ($(document).scrollTop() >= 200) {
+    $('#totop').css("display", "block");
+  } else {
+    $('#totop').css("display", "none");
+  }
+})
